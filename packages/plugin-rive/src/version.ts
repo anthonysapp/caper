1ba009a6a90e3ac8fb0f3667e3c8aad1a59fa36f
anthonysapp <anthonysapp@gmail.com>
2026-07-30 00:46:40 -0400

    chore: regenerate version stamps for 0.2.1

--- packages/plugin-rive/src/version.ts
+++ packages/plugin-rive/src/version.ts
@@ -1,2 +1,2 @@
-export const version = '0.2.0';
+export const version = '0.2.1';
 export const riveVersion = '2.30.1';
--- packages/plugin-rollbar/src/version.ts
+++ packages/plugin-rollbar/src/version.ts
@@ -1,2 +1,2 @@
-export const version = '0.2.0';
+export const version = '0.2.1';
 export const rollbarVersion = '2.26.4';
