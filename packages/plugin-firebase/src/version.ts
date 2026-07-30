6d40cb4f0a0a1bd0d0fc610790215298a86c30ab
anthonysapp <anthonysapp@gmail.com>
2026-07-30 00:16:49 -0400

    refactor: vite bundling / plugins / config

--- packages/plugin-firebase/src/version.ts
+++ packages/plugin-firebase/src/version.ts
@@ -1,2 +1,2 @@
-export const version = '0.1.2';
+export const version = '0.2.0';
 export const firebaseVersion = '10.14.1';
--- packages/plugin-rollbar/src/version.ts
+++ packages/plugin-rollbar/src/version.ts
@@ -1,2 +1,2 @@
-export const version = '0.1.2';
+export const version = '0.2.0';
 export const rollbarVersion = '2.26.4';
