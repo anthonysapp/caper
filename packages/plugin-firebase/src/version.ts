c6ce77c00bc969f6512c270c5d3a42ccf4bec3a0
anthonysapp <anthonysapp@gmail.com>
2026-08-18 11:29:26 -0400

    chore(release): 0.5.1

--- packages/plugin-firebase/src/version.ts
+++ packages/plugin-firebase/src/version.ts
@@ -1,2 +1,2 @@
-export const version = '0.4.1';
+export const version = '0.5.1';
 export const firebaseVersion = '10.14.1';
--- packages/plugin-rollbar/src/version.ts
+++ packages/plugin-rollbar/src/version.ts
@@ -1,2 +1,2 @@
-export const version = '0.4.1';
+export const version = '0.5.1';
 export const rollbarVersion = '2.26.4';
