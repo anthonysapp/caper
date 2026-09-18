52694fa011b0f8b7a71b3921622d3d7add629832
anthonysapp <anthonysapp@gmail.com>
2026-09-18 11:34:54 -0400

    chore(release): 0.7.1

--- packages/core/src/version.ts
+++ packages/core/src/version.ts
@@ -1,3 +1,3 @@
 // This file is auto-generated during the build process.
-export const version: string = '0.7.0';
+export const version: string = '0.7.1';
 export const pixiVersion: string = '8.21.0';
--- packages/plugin-rive/src/version.ts
+++ packages/plugin-rive/src/version.ts
@@ -1,2 +1,2 @@
-export const version = '0.7.0';
+export const version = '0.7.1';
 export const riveVersion = '2.30.1';
